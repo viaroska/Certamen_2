7feb2672d38dfaebbee1ab367b79761f7ef593e6
viaroska <92136376+viaroska@users.noreply.github.com>
2022-11-01 13:55:00 -0300

    3
    agregando nivel

---
+++ Juego_2/Assets/localizador2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class localizador2 : MonoBehaviour
+{
+    public Transform objetivo;
+    public NavMeshAgent agente;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agente = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agente.destination = objetivo.position;
+    }
+}
